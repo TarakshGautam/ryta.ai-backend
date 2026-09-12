179e1fc8f3a52beebfd3e99e4115a5855dc980df
Takshu <codewithtakshu@gmail.com>
2026-09-12 11:53:46 +0530

    Now finally reeady to deploy

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export { default } from "./app";
+import type { NextFunction, Request, Response } from "express";
+import app from "./app";
+import { initializeDatabase } from "./db";
+
+let databaseInitialization: Promise<void> | undefined;
+
+const ensureDatabaseInitialized = () => {
+    if (!databaseInitialization) {
+        databaseInitialization = initializeDatabase().catch((error) => {
+            databaseInitialization = undefined;
+            throw error;
+        });
+    }
+    return databaseInitialization;
+};
+
+const handler = (req: Request, res: Response, next: NextFunction) => {
+    ensureDatabaseInitialized()
+        .then(() => app(req, res, next))
+        .catch(next);
+};
+
+export default handler;
